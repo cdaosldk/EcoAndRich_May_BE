7def95d549f76b3b8924e4cdfed39b9d41f01dc9
Seongjae <cdaosldk@naver.com>
2023-05-08 13:21:28 +0900

    feat: api 아키텍처 구현

--- src/main/java/com/example/ecoandrich_may_be/domain/exchangeRate/controller/ExchangeRateController.java
+++ src/main/java/com/example/ecoandrich_may_be/domain/exchangeRate/controller/ExchangeRateController.java
@@ -1,5 +1,16 @@
 package com.example.ecoandrich_may_be.domain.exchangeRate.controller;
 
+import com.example.ecoandrich_may_be.domain.exchangeRate.service.ExchangeRateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/exchangeRate")
 public class ExchangeRateController {
+
+  private final ExchangeRateService exchangeRateService;
+
 
 }
